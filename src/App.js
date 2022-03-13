b4a80164a4cf4bd902a340dc63350f3bcb50f5ac
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2022-03-12 20:20:41 -0600

    Added Input Box and updated Style

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello!
+          Please enter your username and password.
         </p>
         <form>
-          <label>
-          Name:
-          <input type="text" name="name" class="input-text-app"/>
-          </label>
-          <input type="submit" value="Submit" />
+          <input type="text" name="username" class="input-text-box" placeholder='Username'/>
+          <input type="password" name="password" class="input-text-box" placeholder='Password'/>
+          <br/>
+          <input type="submit" value="Submit" class="button-cool"/>
         </form>
       </header>
     </div>
